a7477579c1d70fbadda726bc2210083aac075277
JP <679434+npgenx@users.noreply.github.com>
2024-11-25 10:02:47 -0800

    tested a build

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    output: 'export', // <=== enables static exports
+    reactStrictMode: true,
+};
+
+export default nextConfig;
