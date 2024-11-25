4921cf4e19c003bcefd95d4523dcd8ee8a3f0306
JP <679434+npgenx@users.noreply.github.com>
2024-11-25 09:15:24 -0800

    testing configs

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     reactStrictMode: true,
     images: {
         unoptimized: true, // Disable default image optimization
-        loader: 'akamai',
-        path: '/project-w/',
+        // loader: 'akamai',
+        // path: '/project-w/',
     },
-    assetPrefix: isProd ? '/project-w/' : '',
-    basePath: isProd ? '/project-w' : '',
-    // output: 'export',
+    // assetPrefix: isProd ? '/project-w/' : '',
+    basePath: process.env.DEPLOYED_GITHUB_PATH || '',
+    output: 'export',
 };
 
 export default nextConfig;
