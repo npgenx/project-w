8e41b4c3ea55c0138492c747e982846430ae315f
JP <679434+npgenx@users.noreply.github.com>
2024-11-25 08:31:01 -0800

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+        loader: 'akamai',
+        path: '/project-w/',
+    },
+    assetPrefix: isProd ? '/project-w/' : '',
+    basePath: isProd ? '/project-w' : '',
+    output: 'export',
 };
 
 export default nextConfig;
